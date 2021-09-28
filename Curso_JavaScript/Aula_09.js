1bd6d706fbe5cf5f05b7987aaae721f2a015aaac
paulofreitas-py <paulofreitas.py@protonmail.com>
2021-09-28 00:14:38 -0300

    Update Aula_09.js

--- Curso_JavaScript/Aula_09.js
+++ Curso_JavaScript/Aula_09.js
@@ -1,4 +1,32 @@
 //SWITCH CASE
 var agora = new Date()
 var diaSem = agora.getDay()
-diaSem
+
+diaSem = 2
+
+switch(diaSem){
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2: 
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('Sábado')
+        break
+    default:
+        console.log('[ERRO 666] Dia Inválido')
+}
+console.log(0)
