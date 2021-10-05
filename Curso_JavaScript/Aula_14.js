d9eed1a72bfda4c702c4c28e90fe69adeb24db46
paulofreitas-py <paulofreitas.py@protonmail.com>
2021-10-04 22:46:20 -0300

    Update Aula_14.js

---
+++ Curso_JavaScript/Aula_14.js
@@ -0,0 +1,16 @@
+// faça um FOR que mostre os números de 0 até 10 somente os PARES
+//(x % 2 == 0) - PAR
+//(x % 2 != 0) - IMPAR
+
+//for(var x = 1; x <= 10; x++){
+//    if ((x % 2) == 0){
+//  console.log(x)
+//    }   
+//}
+
+for (var i = 0; i <= 10; i++) {
+       if (i % 2 == 0) {
+          console.log(`Valor ${i} é divisível por 2`)
+      }
+  }
+    
